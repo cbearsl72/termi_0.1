5e07a18dd778b08212925dbccf5b74fb4bfaa19c
cbearsl72 <jay.muthyala@gmail.com>
2023-11-18 11:50:29 -0800

    Add voiceline functionality and lines.txt used by the pet

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+from random import randint
+
+def load_lines():
+  inputFile = open("lines.txt")
+  lineList = []
+  for line in inputFile:
+    voice = line.split("\n")
+    lineList.append(voice[0])
+  return lineList
+
+voicelines = []
+voicelines = load_lines()
+randIndex = randint(0, len(voicelines) - 1);
+print(voicelines[randIndex])
